50b5025ac0076c583ce07dbad986e94836640d1b
William Daniels <williambdaniels@gmail.com>
2013-11-01 22:49:59 -0600

    Maybe this'll work? :)

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 # This function reads your DATABASE_URL configuration automatically set by Heroku
 # the return value is a string that will work with pg_connect
 function pg_connection_string() {
-  return "dbname=dfncb0i2lm0m7q ec2-54-235-152-22.compute-1.amazonaws.com port=5432 user=dzpmeljnudwqtm password=tg6BVdzlv9zwagqsEim-GL9J15 sslmode=require";
+  return "dbname=dfncb0i2lm0m7q host=ec2-54-235-152-22.compute-1.amazonaws.com port=5432 user=dzpmeljnudwqtm password=tg6BVdzlv9zwagqsEim-GL9J15 sslmode=require";
 }
  
 # Establish db connection
