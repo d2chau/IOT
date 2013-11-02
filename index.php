9174c07711320897e01d0c4ff63f8bdb7a638f65
William Daniels <williambdaniels@gmail.com>
2013-11-01 21:57:55 -0600

    HAHAHAHAHAHAHAH:wq

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 <?php
 echo "Hello world";
 phpinfo();
-#$con=mysqli_connect("mysql8.000webhost.com","a7342679_frm8","Apple12","a7342679_frm8");
+$con=mysqli_connect("mysql8.000webhost.com","a7342679_frm8","Apple12","a7342679_frm8");
 # Establish db connection
-#echo "Trying to connect to the database and stuff. shit. ";
+echo "Trying to connect to the database and stuff. shit. ";
 #if (mysql_connect_errno($con){
 #   echo "Database connection error.";
 #   exit;
