04b484973ce380de216a4cd7158f4ec23518e18a
William Daniels <williambdaniels@gmail.com>
2013-11-01 22:42:06 -0600

    Woohoo! we're making progress!

--- index.php
+++ index.php
@@ -2,23 +2,21 @@
 <html>
 <body>
 <?php
-echo "Hello world";
-$con=mysqli_connect("mysql8.000webhost.com","a7342679_frm8","Apple12","a7342679_frm8");
+# This function reads your DATABASE_URL configuration automatically set by Heroku
+# the return value is a string that will work with pg_connect
+function pg_connection_string() {
+  return "dbname=dfncb0i2lm0m7q ec2-54-235-152-22.compute-1.amazonaws.com port=5432 user=dzpmeljnudwqtm password=tg6BVdzlv9zwagqsEim-GL9J15 sslmode=require";
+}
+ 
 # Establish db connection
-echo "Trying to connect to the database and stuff. shit. ";
-if (mysql_connect_errno($con)){
-   echo "Database connection error.";
+$db = pg_connect(pg_connection_string());
+if (!$db) {
+   echo "Database connection error."
    exit;
 }
-# 
-#$result = mysqli_query($con, "SELECT * FROM test");#
-#
-#while($row = mysqli_fetch_array($result))
-#  {
-#  echo $row['id'] . " " . $row['foo'];
-#  echo "<br>";
-#  echo "in the while loop";
-#  }
-?> 
+ 
+$result = pg_query($db, "TABLE test");
+echo $result;
+?>
 </body>
 </html>
