1d85230ab16812ab8990fb29eb3458f75cacf61a
William Daniels <williambdaniels@gmail.com>
2013-11-02 09:55:22 -0600

    k;isjdaf

--- index.php
+++ index.php
@@ -21,19 +21,19 @@
          <p><a class="btn btn-primary btn-large">Get Started! &raquo;</a></p>
          </div><!-- .hero-unit -->
   <div class="row-fluid">
-         <div class="span4">
+         <div class="innerDiv">
          <h2>Box Number 1</h2>
          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
          <p><a class="btn" href="#">Click meeee &raquo;</a></p>
          </div><!-- .span4 -->
    
-         <div class="span4">
+         <div class="innerDiv">
                  <h2>Box Number 2</h2>
          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
          <p><a class="btn" href="#">Click meeee &raquo;</a></p>
          </div><!-- .span4 -->
    
-         <div class="Database">
+         <div class="innerDiv">
          	<style>
 				body { 
             float:left;}
