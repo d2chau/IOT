136510cdcf494eac2e88c53364013efaf1fcb2e0
William Daniels <williambdaniels@gmail.com>
2013-11-01 21:59:42 -0600

    A;ODJFA;LKDJF;ALKJF

--- index.php
+++ index.php
@@ -3,16 +3,15 @@
 <body>
 <?php
 echo "Hello world";
-phpinfo();
 $con=mysqli_connect("mysql8.000webhost.com","a7342679_frm8","Apple12","a7342679_frm8");
 # Establish db connection
 echo "Trying to connect to the database and stuff. shit. ";
-#if (mysql_connect_errno($con){
-#   echo "Database connection error.";
-#   exit;
-#}
+if (mysql_connect_errno($con){
+   echo "Database connection error.";
+   exit;
+}
 # 
-#$result = pg_query($con, "SELECT * FROM test");#
+#$result = mysqli_query($con, "SELECT * FROM test");#
 #
 #while($row = mysqli_fetch_array($result))
 #  {
