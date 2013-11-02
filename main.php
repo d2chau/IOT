bdba62ce4dd5a0ed34ac294e3aea8116de815bd5
William Daniels <williambdaniels@gmail.com>
2013-11-02 10:40:52 -0600

    t

--- main.php
+++ main.php
@@ -12,31 +12,13 @@
     <script src="http://code.jquery.com/jquery.js"></script>
     <script src='bootstrap/app/www/js/bootstrap.min.js'></script>
 
-     <div class="willscontainer">
-         
-         <div class="hero-unit">
-         <h1>Fridgem8</h1>
-         <p>Welcome to Fridgem8! A list that shops the way YOU want. Click below to get started. </p>
-         <p><a class="btn btn-primary btn-large">Get Started! &raquo;</a></p>
-         </div><!-- .hero-unit -->
-  <div class="row-fluid">
-         <div class="innerDiv">
-         <h2>Box Number 1</h2>
-         <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-         <p><a class="btn" href="#">Click meeee &raquo;</a></p>
-         </div><!-- .span4 -->
-         <div class="innerDiv">
-                 <h2>Box Number 2</h2>
-         <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-         <p><a class="btn" href="#">Click meeee &raquo;</a></p>
-         </div><!-- .span4 -->
-         <div class="innerDiv">
-                 <h2>Database Query Test</h2>
-         <p>This should be the DB query that we're playing with for now: </p>
-         <p><a class="btn" href="#">Click meeee &raquo;</a></p>
-         </div><!-- .span4 -->
- 
- </div><!-- .row -->
+ <div class="willscontainer">
+     <div class="row-fluid">
+        <div class="leftPane">
+        </div>
+        <div class="rightPane">
+        </div>
+     </div>
  </div><!-- .container -->
 </body>
 </html>
