8599f0a6e9520d8d19ad6c311fa65169f710265e
William Daniels <williambdaniels@gmail.com>
2013-11-01 21:44:01 -0600

    Ugh, spam

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 
 <?php
-# This function reads your DATABASE_URL configuration automatically set by Heroku
-# the return value is a string that will work with pg_connect
-function pg_connection_string() {
-  // we will fill this out next
-}
+echo "Hello world";
 $con=mysqli_connect("mysql8.000webhost.com","a7342679_frm8","Apple12","a7342679_frm8");
 # Establish db connection
 echo "Trying to connect to the database and stuff. shit. "
@@ -19,6 +15,7 @@ while($row = mysqli_fetch_array($result))
   {
   echo $row['id'] . " " . $row['foo'];
   echo "<br>";
+  echo "in the while loop";
   }
 
 
