d0dbb5e8206b1edd3ca6b74c3f4ceaab0e099c70
William Daniels <williambdaniels@gmail.com>
2013-11-01 21:54:27 -0600

    shitty

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 <body>
 <?php
 echo "Hello world";
-phpinfo()
+phpinfo();
 $con=mysqli_connect("mysql8.000webhost.com","a7342679_frm8","Apple12","a7342679_frm8");
 # Establish db connection
-echo "Trying to connect to the database and stuff. shit. "
+echo "Trying to connect to the database and stuff. shit. ";
 if (mysql_connect_errno($con){
-   echo "Database connection error."
+   echo "Database connection error.";
    exit;
 }
  
