b042e9b210ade33f9d08389bc15a123a3c105d84
William Daniels <williambdaniels@gmail.com>
2013-11-02 09:37:13 -0600

    kjhdfsif

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title>Fridgem8</title>
          <meta charset="utf-8">
          <title>FridgeM8</title>
-          <link rel="stylesheet" href= "bootstrap/app/www/css/main.css" type="text/css">
+          <link rel="stylesheet" href= "bootstrap/app/www/css/main.css">
          <link rel="stylesheet" href="http://flip.hr/css/bootstrap.min.css">
   </head>
 <body>
