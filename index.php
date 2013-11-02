28e806bc1ca0632903591f686e496f7642cf0f94
William Daniels <williambdaniels@gmail.com>
2013-11-01 21:46:21 -0600

    So much spam, ughhh.

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 
+<html>
+<body>
 <?php
 echo "Hello world";
+phpinfo()
 $con=mysqli_connect("mysql8.000webhost.com","a7342679_frm8","Apple12","a7342679_frm8");
 # Establish db connection
 echo "Trying to connect to the database and stuff. shit. "
@@ -20,4 +23,5 @@ while($row = mysqli_fetch_array($result))
 
 
 ?> 
-
+</body>
+</html>
