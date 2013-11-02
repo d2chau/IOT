161849a4c3e4559e0469d02b91443aed96d768bf
William Daniels <williambdaniels@gmail.com>
2013-11-02 08:04:25 -0600

    l;ajdf

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
      <div class="container">
          
          <div class="hero-unit">
-         <h1><?php
+         <h1>Matt is cool<?php
 # This function reads your DATABASE_URL configuration automatically set by Heroku
 # the return value is a string that will work with pg_connect
 function pg_connection_string() {
